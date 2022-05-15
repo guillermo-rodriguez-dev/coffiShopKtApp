dcf0d629a89d7e631fd2c854db1bb80525b8e63f
Guillermo Rodriguez <48388462+guille-developer@users.noreply.github.com>
2022-05-14 22:30:49 -0300

    se crea custom app bar

--- app/src/main/java/com/platzi/coffeshop/ui/components/CustomAppBar.kt
+++ app/src/main/java/com/platzi/coffeshop/ui/components/CustomAppBar.kt
@@ -1,4 +1,48 @@
 package com.platzi.coffeshop.ui.components
 
-class CustomAppBar {
+import android.widget.ImageButton
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.tooling.preview.Preview
+import com.platzi.coffeshop.ui.theme.CoffeShopTheme
+
+
+typealias NavegationAction = () -> Unit
+@Composable
+fun CustomAppBar(
+    title: String? = null,
+    navigationIcon: ImageVector? = null,
+    navigationAction: NavegationAction?=null
+) {
+    val titleText = title ?: ""
+    if(navigationIcon!=null &&navigationAction != null){
+        TopAppBar(
+            title = { Text(text = titleText) },
+            backgroundColor = MaterialTheme.colors.primary,
+            navigationIcon = {
+                IconButton(onClick = {
+
+                }){
+                    Icon(imageVector = navigationIcon, "")
+                }
+            }
+        )
+    }else{
+        TopAppBar(
+            title = { Text(text = titleText) },
+            backgroundColor = MaterialTheme.colors.primary
+        )
+
+
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CustomAppBarPreview() {
+    CoffeShopTheme {
+        CustomAppBar(title = "CoffeForCodders")
+    }
 }
--- app/src/main/java/com/platzi/coffeshop/ui/screens/DetailsScreen.kt
+++ app/src/main/java/com/platzi/coffeshop/ui/screens/DetailsScreen.kt
@@ -2,24 +2,44 @@ package com.platzi.coffeshop.ui.screens
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Button
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.platzi.coffeshop.ui.components.CountryISO
+import com.platzi.coffeshop.ui.components.CustomAppBar
 
 @Composable
 fun DetailsScreen(navController: NavController, countryISO: CountryISO) {
-    Column() {
-        Text(text = "$countryISO.iso", color = Color.Green)
-        Button(onClick = { navController.navigate("feed") {
-            popUpTo("feed")
-        } }) {
 
+    Scaffold(
+        topBar = { CustomAppBar(
+            title= countryISO.name,
+            navigationIcon = Icons.Filled.ArrowBack,
+          navigationAction = {
+              navController.navigate("feed") {
+                  popUpTo("feed")
+              }
+          } ) },
+        content = {
+            Column() {
+
+                Text(text = "$countryISO.iso", color = Color.Green)
+                Button(onClick = { navController.navigate("feed") {
+                    popUpTo("feed")
+                } }) {
+
+                }
+            }
         }
-    }
+
+    )
+
 }
 
 
